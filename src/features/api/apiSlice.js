6c3965068106b94c3d2fb5b62b20d5bc2ef1801d
pradhyumang <pradyumang5@gmail.com>
2024-05-08 18:23:44 +0530

    rtk-Query

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -7,6 +7,7 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
       providesTags: ["Todos"],
     }),
     addTodo: builder.mutation({
@@ -23,6 +24,7 @@ export const apiSlice = createApi({
         method: "PATCH",
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     deleteTodo: builder.mutation({
       query: ({ id }) => ({
@@ -30,6 +32,7 @@ export const apiSlice = createApi({
         method: "DELETE",
         body: id,
       }),
+      invalidatesTags: ["Todos"],
     }),
   }),
 });
